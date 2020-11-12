cc7ff60448d6d8d3b36b57bd4b93cf6ec9388c84
Alisher Yessenkozha <alisher1.618@gmail.com>
2020-11-12 22:29:07 +0600

    created navBar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <nav-bar />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import NavBar from "../components/Main/NavBar";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    NavBar
   }
 };
 </script>
